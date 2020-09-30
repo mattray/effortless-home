8ffeba7ec01cbc7c074b277aaa46f7736723f9a8
Matt Ray <github@mattray.dev>
2020-09-30 16:50:27 +1000

    add proper branch, works locally

--- audit-base/habitat/plan.sh
+++ audit-base/habitat/plan.sh
@@ -3,7 +3,4 @@ pkg_origin=mattray
 pkg_version=0.2.0
 pkg_maintainer="Matt Ray"
 pkg_license=("Apache-2.0")
-pkg_scaffolding="chef/scaffolding-chef-inspec/0.22.0/20200929143624"
-pkg_deps=(
-  core/cacerts/2020.07.22/20200924114313
-)
+pkg_scaffolding="chef/scaffolding-chef-inspec"
