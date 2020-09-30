d5a4ab97dc50a3e70979c6a07681b632ebbddcab
Matt Ray <github@mattray.dev>
2020-09-30 16:07:06 +1000

    update to latest(?) scaffolding

--- audit-base/habitat/plan.sh
+++ audit-base/habitat/plan.sh
@@ -1,6 +1,6 @@
 pkg_name=effortless-audit-base
 pkg_origin=mattray
-pkg_version=0.1.9
+pkg_version=0.2.0
 pkg_maintainer="Matt Ray"
 pkg_license=("Apache-2.0")
-pkg_scaffolding="chef/scaffolding-chef-inspec/0.21.0/20200707104154"
+pkg_scaffolding="chef/scaffolding-chef-inspec"
