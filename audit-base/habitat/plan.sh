d2a75a5b23db216c872d41110376c27fb14a0b44
Matt Ray <github@mattray.dev>
2020-09-30 16:31:32 +1000

    update cacerts?

--- audit-base/habitat/plan.sh
+++ audit-base/habitat/plan.sh
@@ -4,3 +4,6 @@ pkg_version=0.2.0
 pkg_maintainer="Matt Ray"
 pkg_license=("Apache-2.0")
 pkg_scaffolding="chef/scaffolding-chef-inspec/0.22.0/20200929143624"
+pkg_deps=(
+  core/cacerts/2020.07.22/20200924114313
+)
