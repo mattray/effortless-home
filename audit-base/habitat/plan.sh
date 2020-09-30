25e1655fb2199cd9ec327b262c52ab6e5d5e558a
Matt Ray <github@mattray.dev>
2020-09-30 16:16:22 +1000

    inspec scaffolding not stable yet?

--- audit-base/habitat/plan.sh
+++ audit-base/habitat/plan.sh
@@ -3,4 +3,4 @@ pkg_origin=mattray
 pkg_version=0.2.0
 pkg_maintainer="Matt Ray"
 pkg_license=("Apache-2.0")
-pkg_scaffolding="chef/scaffolding-chef-inspec"
+pkg_scaffolding="chef/scaffolding-chef-inspec/0.22.0/20200929143624"
